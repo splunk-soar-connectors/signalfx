3c1dc3e76f2de076cd2fe7d9877676f1b7b90412
Ian Rokas <grokas@splunk.com>
2025-02-28 20:25:43 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- signalfx_view.py
+++ signalfx_view.py
@@ -1,6 +1,6 @@
 # File: signalfx_view.py
 #
-# Copyright (c) 2021-2023 Splunk Inc.
+# Copyright (c) 2021-2025 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 def get_ctx_result(provides, result):
-    """ Function that parses data.
+    """Function that parses data.
 
     :param result: result
     :param provides: action name
@@ -24,20 +24,20 @@ def get_ctx_result(provides, result):
     param = result.get_param()
     data = result.get_data()
 
-    ctx_result['param'] = param
+    ctx_result["param"] = param
 
-    ctx_result['action'] = provides
+    ctx_result["action"] = provides
 
     if not data:
-        ctx_result['data'] = []
+        ctx_result["data"] = []
         return ctx_result
-    ctx_result['data'] = data
+    ctx_result["data"] = data
 
     return ctx_result
 
 
 def display_view(provides, all_app_runs, context):
-    """ Function that displays view.
+    """Function that displays view.
 
     :param provides: action name
     :param context: context
@@ -45,7 +45,7 @@ def display_view(provides, all_app_runs, context):
     :return: html page
     """
 
-    context['results'] = results = []
+    context["results"] = results = []
     for _, action_results in all_app_runs:
         for result in action_results:
             ctx_result = get_ctx_result(provides, result)
